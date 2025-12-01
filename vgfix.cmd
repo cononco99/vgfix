a56ca21ff0a1eda422ae2bd65e7af0fd0ccc4892
cononco99 <cononco99@gmail.com>
2025-12-01 14:34:33 -0800

    working now

--- vgfix.cmd
+++ vgfix.cmd
@@ -1,27 +1,30 @@
 @SETLOCAL ENABLEEXTENSIONS
 @perl.exe -x "%~f0" %*
 @exit /b %ERRORLEVEL%
+
+## thank you ijprest for the above scripting trick (https://stackoverflow.com/a/1056231/509928)
+
 #!perl
 
-# thank you ijprest for the above scripting trick (https://stackoverflow.com/a/1056231/509928)
+use strict; 
+use warnings;
+use File::Basename;
 
-use FindBin;  # This is a module that finds the location of your script
-BEGIN { chdir $FindBin::Bin }  # set context to that directory.
+#if $#ARGV != 0 die "expected 1 filename argument.";
+
+my $input_file=$ARGV[0];
+
+my($filename, $dirs, $suffix) = fileparse($input_file);
+my $output_file= $dirs . "vgfix_" . $filename . $suffix;
+
+open(INPUT, '<', $input_file) or die "could not open $input_file";
+open(OUTPUT, '>', $output_file) or die "could not open $output_file";
 
 
-open OUTPUT, ">o.txt";
-print OUTPUT "This is a test\n";
-
-my $total = $#ARGV + 1;
-my $counter = 1;
- 
-# get script name
-my $scriptname = $0;
- 
-print OUTPUT "Total args passed to $scriptname : $total\n";
- 
-# Use loop to print all args stored in an array called @ARGV
-foreach my $a(@ARGV) {
-	print OUTPUT "Arg # $counter : $a\n";
-	$counter++;
+while (<INPUT>) {
+  $_ =~ s/<INCOMETYPE>REINVEST/<INCOMETYPE>DIV/;
+  print OUTPUT;
 }
+
+close (INPUT); 
+close (OUTPUT);
