4aa6ff7a4e2ac510b3aaabbc861507ee17fc6b4e
cononco99 <cononco99@gmail.com>
2025-12-01 15:03:12 -0800

    minor cleanup

--- vgfix.cmd
+++ vgfix.cmd
@@ -1,25 +1,25 @@
 @SETLOCAL ENABLEEXTENSIONS
 @perl.exe -x "%~f0" %*
 @exit /b %ERRORLEVEL%
+# Thanks to ijprest for the scripting trick (https://stackoverflow.com/a/1056231/509928)
 
-## thank you ijprest for the above scripting trick (https://stackoverflow.com/a/1056231/509928)
-
+# To run from command line, delete lines up to and including this line.
 #!perl
 
 use strict; 
 use warnings;
 use File::Basename;
 
-#if $#ARGV != 0 die "expected 1 filename argument.";
+if ($#ARGV != 0) {
+    die "expected exactly 1 filename argument.";
+}
 
 my $input_file=$ARGV[0];
-
 my($filename, $dirs, $suffix) = fileparse($input_file);
 my $output_file= $dirs . "vgfix_" . $filename . $suffix;
 
 open(INPUT, '<', $input_file) or die "could not open $input_file";
 open(OUTPUT, '>', $output_file) or die "could not open $output_file";
-
 
 while (<INPUT>) {
   $_ =~ s/<INCOMETYPE>REINVEST/<INCOMETYPE>DIV/;
